bbe9c8a2978a83fb2e5ed4cde0a5e4aafe736188
dev-nasreen <nasreen.akter34@gmail.com>
2022-09-28 19:55:54 +0600

    html mark up done

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        id:1,
+        year: 1914,
+        events: "- Born in Cresco, Iowa"
+    },
+    {
+        id:2,
+        year: 1933,
+        events: `- Leaves his family's farm to attend the University of Minnesota, thanks to a Depression era program known as the "National Youth Administration"`
+    },
+    {
+        id:3,
+        year: 1935,
+        events: ` - Has to stop school and save up more money.
+        Works in the Civilian Conservation Corps, helping starving
+        Americans. "I saw how food changed them", he said. "All of this left
+        scars on me."`
+    },
+    {
+        id:4,
+        year: 1937,
+        events: "- Finishes university and takes a job in the US Forestry Service"
+    },
+    {
+        id:5,
+        year: 1938,
+        events: "- Marries wife of 69 years Margret Gibson. Gets laid off due to budget cuts. Inspired by Elvin Charles Stakman, he returns to school study under Stakman, who teaches him about breeding pest-resistent plants."
+    },
+    {
+        id:6,
+        year: 1941,
+        events: "- Tries to enroll in the military after the Pearl Harbor attack, but is rejected. Instead, the military asked his lab to work on waterproof glue, DDT to control malaria, disinfectants, and other applied science."
+    },
+]
